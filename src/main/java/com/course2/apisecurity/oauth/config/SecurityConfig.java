7ed6c32a2469846ab7c0363cbcaace7444257da5
helen <hhma@alumni.cpp.edu>
2022-08-25 22:19:59 -0400

    replace WebSecurityConfigurerAdapter

--- src/main/java/com/course2/apisecurity/oauth/config/SecurityConfig.java
+++ src/main/java/com/course2/apisecurity/oauth/config/SecurityConfig.java
@@ -1,16 +1,17 @@
 package com.course2.apisecurity.oauth.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig   {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
        // http.antMatcher("/**").authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated().and()
        //        .oauth2Login();
 
@@ -19,5 +20,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/math/random").permitAll()
                 .anyRequest()
                 .authenticated().and().oauth2Login();
+        return http.build();
     }
 }
