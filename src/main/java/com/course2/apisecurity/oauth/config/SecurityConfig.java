423ec8f779f32d8b97ac52cb41b27e3a68ccd5df
helen <hhma@alumni.cpp.edu>
2022-08-26 23:35:33 -0400

    use WebSecurityConfigureAdapter

--- src/main/java/com/course2/apisecurity/oauth/config/SecurityConfig.java
+++ src/main/java/com/course2/apisecurity/oauth/config/SecurityConfig.java
@@ -1,25 +1,21 @@
 package com.course2.apisecurity.oauth.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig   {
+public class SecurityConfig   extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-       // http.antMatcher("/**").authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated().and()
-       //        .oauth2Login();
+    @Override
+    public void configure (HttpSecurity http) throws Exception {
+        http.antMatcher("/**").authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated().and()
+              .oauth2Login();
 
-		http.antMatcher("/**")
-                .authorizeRequests()
-                .antMatchers("/api/math/random").permitAll()
-                .anyRequest()
-                .authenticated().and().oauth2Login();
-        return http.build();
     }
 }
